70bdfc841c7d8ca74a0d5016dc51c400b2931c4a
Xenia Voellmy <xenia@student-net-vpn-2479.ethz.ch>
2020-03-05 17:31:14 +0100

    Intermediate state, not finished.

--- task_1/task_1a/main.py
+++ task_1/task_1a/main.py
@@ -1,21 +1,58 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Ridge
 import pandas as pd
 
+def read_in_data(file):
+
+    #read data from csv file and transfer to numpy array
+    read_train = pd.read_csv('../../data_1/data_1a/' + file, delimiter=',')  # read in train.csv file
+    data_train = read_train.to_numpy()
+
+    #initialize the used arrays
+    Id = []
+    y = []
+    X = []
+
+    for row in data_train:
+        Id.append(int(row[0]))
+        y.append(row[1])
+        X.append(list(row[2:]))
+
+    return [Id,y,X]
+
+def make_batches(Id, number_of_batches):
+    size = len(Id)
+    batches = [[ 0 for i in range(0)] for i in range(number_of_batches)]
+
+    for i in range(number_of_batches):
+        for j in range(size):
+            if Id[j]%number_of_batches==i:
+                batches[i].append(Id[j])
+
+    return batches
+
+def ridge_regression(X, y, alpha):
+    w_star = np.dot(np.linalg.inv(np.dot(np.transpose(X), X) + alpha * np.identity(len(np.transpose(X)))), np.dot(np.transpose(X), y))
+    return w_star
+
+
 #The file value is either "train.csv" or "test.csv"
-file = 'train.csv'
+[Id, y, X] = read_in_data('train.csv')
+number_of_batches = 10
+batches = make_batches(Id,number_of_batches)
 
-#read data from csv file and transfer to numpy array
-read_train = pd.read_csv('../../data_1/data_1a' + file, delimiter=',')  # read in train.csv file
-data_train = read_train.to_numpy()
-
-#initialize the used arrays
-Id = []
-y = []
-y_pred = []
-X = []
-
-for row in data_train:
-    Id.append(int(row[0]))
-    y.append(row[1])
-    X.append(list(row[2:]))
+for alpha in [0.01, 0.1, 1, 10, 100]:
+    for i in range(number_of_batches):
+        X_red = X
+        y_red = y
+        X_val = []
+        y_val = []
+        for id in batches[i]:
+            del(X_red[id])
+            del(y_red[id])
+            X_val.append(X[id])
+            y_val.append(y[id])
+        w = ridge_regression(X_red, y_red, alpha)
+        y_pred = np.dot(np.transpose(w), np.transpose(X_val))
+        rmse = np.sqrt(mean_squared_error(y_val, y_pred))
