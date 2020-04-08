04d77c36a1a05a0a89c50be19f24e0f20d40947a
Rafael Fröhlich <rafael.froehlich@bluewin.ch>
2020-04-08 08:39:30 +0200

    take full length of test features

--- task_2/main.py
+++ task_2/main.py
@@ -13,8 +13,8 @@ def read_in_data():
     read_train_features = read_train_features.replace('nan', np.NaN)
     mean_train = np.array(read_train_features.mean())
     std_train = np.array(read_train_features.std())
-    number_of_patients = int(read_train_features.shape[0]/12)
-    [X_TRAIN, pid_train] = pre_processing(number_of_patients, mean_train, std_train, read_train_features)
+    number_train_patients = int(read_train_features.shape[0]/12)
+    [X_TRAIN, pid_train] = pre_processing(number_train_patients, mean_train, std_train, read_train_features)
 
     #read train labels (Y_LABELS)
     read_train_labels = pd.read_csv('../data_2/train_labels.csv', delimiter=',', nrows=line_numbers)
@@ -29,11 +29,12 @@ def read_in_data():
         Y_LABELS_3.append(list(row[12:16]))
 
     #read test data (X_TEST)
-    read_test_features = pd.read_csv('../data_2/test_features.csv', delimiter=',', nrows=(line_numbers*12))
+    read_test_features = pd.read_csv('../data_2/test_features.csv', delimiter=',')
     read_test_features = read_test_features.replace('nan', np.NaN)
     mean_test = np.array(read_test_features.mean())
     std_test = np.array(read_test_features.std())
-    [X_TEST, pid_test] = pre_processing(number_of_patients, mean_test, std_test, read_test_features)
+    number_test_patients = int(read_test_features.shape[0]/12)
+    [X_TEST, pid_test] = pre_processing(number_test_patients, mean_test, std_test, read_test_features)
 
     return [pid_train, pid_test, Y_LABELS_1, Y_LABELS_2, Y_LABELS_3, X_TRAIN, X_TEST]
 
@@ -71,17 +72,14 @@ model_2 = svm.SVC(kernel='linear', probability=True)
 model_2.fit(X_TRAIN, Y_LABELS_2)
 y_pred_2 = model_2.predict_proba(X_TEST)[:,1]                                   #Prediction
 
-
 ##Subtask 3: Setting up a model for mean of vital signs
 model_3 = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1], cv=None)                        #None for Leave-One-Out cross-validation
 model_3.fit(X_TRAIN, Y_LABELS_3)
 y_pred_3 = model_3.predict(X_TEST)                                              #Prediction
 
-
 #Creating submission matrix and writing to zip
 M_Sub = np.c_[pid_test, y_pred_1, y_pred_2, y_pred_3]
 M_Sub_panda = pd.DataFrame(data=M_Sub, columns=["pid","LABEL_BaseExcess","LABEL_Fibrinogen","LABEL_AST","LABEL_Alkalinephos","LABEL_Bilirubin_total","LABEL_Lactate","LABEL_TroponinI","LABEL_SaO2","LABEL_Bilirubin_direct","LABEL_EtCO2","LABEL_Sepsis","LABEL_RRate","LABEL_ABPm","LABEL_SpO2","LABEL_Heartrate"])
-print(M_Sub_panda)
 
 compression_opts = dict(method='zip', archive_name='sample.csv')
 M_Sub_panda.to_csv('sample.zip', index=False, float_format='%.3f', compression=compression_opts)
--- task_2/main_try.py
+++ task_2/main_try.py
@@ -1,102 +1,92 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.linear_model import RidgeCV
 from sklearn import svm
-from scipy.special import expit
 import pandas as pd
-import inspect
 
 #READ DATA FROM CSV FILE AND TRANSFER TO NUMPY ARRAY
 def read_in_data():
-    #read train features (X_FEATURES)
-    read_train_features = pd.read_csv('../data_2/train_features.csv', delimiter=',', nrows=120)
+    #read train features (X_TRAIN)
+    line_numbers = 18995                                                          #maximum number = 18995, minimum number = 100
+    read_train_features = pd.read_csv('../data_2/train_features.csv', delimiter=',', nrows=(line_numbers*12))
     read_train_features = read_train_features.replace('nan', np.NaN)
-    read_train_features.fillna(read_train_features.mean(), inplace=True)        #Dealing with "nan" values --> replacing them with the mean value of the whole column
-    data_train_features = read_train_features.to_numpy()
+    mean_train = np.array(read_train_features.mean())
+    std_train = np.array(read_train_features.std())
+    number_of_patients = int(read_train_features.shape[0]/12)
+    [X_TRAIN, pid_train] = pre_processing(number_of_patients, mean_train, std_train, read_train_features)
 
-    X_FEATURES = []
-    for row in data_train_features:                                             #from Age (column C) to pH (column AK)
-        X_FEATURES.append(list(row[2:]))
+    #read train labels (Y_LABELS)
+    read_train_labels = pd.read_csv('../data_2/train_labels.csv', delimiter=',', nrows=line_numbers)
+    data_train_labels = np.array(read_train_labels)
 
-    #read train labels (pid and Y_LABELS)
-    read_train_labels = pd.read_csv('../data_2/train_labels.csv', delimiter=',', nrows=10)
-    data_train_labels = read_train_labels.to_numpy()
-
-    pid = []
-    Y_LABELS = []
+    Y_LABELS_1 = []
+    Y_LABELS_2 = []
+    Y_LABELS_3 = []
     for row in data_train_labels:
-        pid.append(int(row[0]))
-        Y_LABELS.append(list(row[1:11]))
+        Y_LABELS_1.append(list(row[1:11]))
+        Y_LABELS_2.append(float(row[11]))
+        Y_LABELS_3.append(list(row[12:16]))
 
     #read test data (X_TEST)
-    read_test_features = pd.read_csv('../data_2/test_features.csv', delimiter=',', nrows=120)
+    read_test_features = pd.read_csv('../data_2/test_features.csv', delimiter=',', nrows=(line_numbers*12))
     read_test_features = read_test_features.replace('nan', np.NaN)
-    read_test_features.fillna(read_test_features.mean(), inplace=True)          #Dealing with "nan" values --> replacing them with the mean value of the whole column
-    data_test_features = read_test_features.to_numpy()
+    mean_test = np.array(read_test_features.mean())
+    std_test = np.array(read_test_features.std())
+    [X_TEST, pid_test] = pre_processing(number_of_patients, mean_test, std_test, read_test_features)
 
-    X_TEST = []
-    for row in data_test_features:
-        X_TEST.append(list(row[2:]))
+    return [pid_train, pid_test, Y_LABELS_1, Y_LABELS_2, Y_LABELS_3, X_TRAIN, X_TEST]
 
-    return [pid, Y_LABELS, X_FEATURES, X_TEST]
-
-def pre_processing(pid, X_LONG):
+def pre_processing(number_of_patients, mean, std, data_set):
     X_CUT = []
-    for i in range(1,len(pid)+1):                                               #going from 1 to the maximum number of patients
-        X_Patient = X_LONG[(1+12*(i-1)):(12*i)]                             #take only the relevant data for that specific patient
-        X_CUT.append(list(np.mean(X_Patient, axis=0)))                               #axis=0 for taking the mean value over columns instead of rows
+    pid = []
+    for i in range(0,number_of_patients):                                       #going from 1 through all patients
+        X_Patient = data_set.iloc[(12*i):(12*(i+1)),:]                          #extract only the relevant data for that specific patient
+        X_append = X_Patient.mean()
+        X_append = np.array(X_append)                                           #Has still all the columns in it
+        pid.append(X_append[0])
+        X_append = X_append[2:]                                                 #cut away the pid and the time (start from Age)
 
-    X_CUT = np.nan_to_num(X_CUT)                                              #OPTIONAL: convert all nan's to 0.0 (should not be necessary in the end anymore...)
+        for j in range(0,len(X_append)):
+            if np.isnan(X_append[j]):
+                np.random.seed(1)
+                X_append[j] = np.random.normal(mean[j+2], std[j+2], 1)
+        X_CUT.append(list(X_append))
 
-    return X_CUT
-
-def predict(X_TEST_cut):            #TODO!!!
-    return expit(X_TEST_cut)
+    return [X_CUT, pid]
 
 
 ###---------------MAIN----------------------------------------------------------
-[pid, Y_LABELS, X_FEATURES, X_TEST] = read_in_data()                            #Read the data from features file
-
-#nans have been replaced and now the X_FEATURES is shrinked to only one row per patient using mean approximation
-X_TRAIN = pre_processing(pid, X_FEATURES)                                        #from now on work with X_DATA
+[pid_train, pid_test, Y_LABELS_1, Y_LABELS_2, Y_LABELS_3, X_TRAIN, X_TEST] = read_in_data()            #Read the data from features file
+X_TEST = np.nan_to_num(X_TEST)#random stuff:)
 
 ##Subtask 1: Setting up a model with multiclass labels
-model = OneVsRestClassifier(svm.SVC(kernel='linear'))
-#print(X_DATA)
-model.fit(X_TRAIN, Y_LABELS)
+y_pred_1 = []
+Y_LABELS_1 = np.array(Y_LABELS_1)                                               #convert to numpy array, because an element of the list cannot be accessed with [:,column]
+for column in range(0,10):
+    model_1 = svm.SVC(kernel='linear', probability=True)
+    model_1.fit(X_TRAIN, Y_LABELS_1[:,column])
+    if column == 0:
+        y_pred_1 = model_1.predict_proba(X_TEST)[:,1]
+    else:
+        y_pred_1 = np.c_[y_pred_1, model_1.predict_proba(X_TEST)[:,1]]               #Prediction
+#print(y_pred_1)
 
-X_VALID = pre_processing(pid, X_TEST)
-#Prediction
-y_pred = model.predict(X_VALID)
-print(y_pred)
-#y_mypred = predict(X_VALID)
-#print(y_mypred)
+##Subtask 2: Setting up a model for sepsis
+model_2 = svm.SVC(kernel='linear', probability=True)
+model_2.fit(X_TRAIN, Y_LABELS_2)
+y_pred_2 = model_2.predict_proba(X_TEST)[:,1]                                   #Prediction
+#print(y_pred_2)
 
-#for i in range(0,n_cutoff):
-#    print(y_pred_cut[12*i])
+##Subtask 3: Setting up a model for mean of vital signs
+model_3 = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1], cv=None)                        #None for Leave-One-Out cross-validation
+model_3.fit(X_TRAIN, Y_LABELS_3)
+y_pred_3 = model_3.predict(X_TEST)                                              #Prediction
+print(y_pred_3)
 
+#Creating submission matrix and writing to zip
+M_Sub = np.c_[pid_test, y_pred_1, y_pred_2, y_pred_3]
+M_Sub_panda = pd.DataFrame(data=M_Sub, columns=["pid","LABEL_BaseExcess","LABEL_Fibrinogen","LABEL_AST","LABEL_Alkalinephos","LABEL_Bilirubin_total","LABEL_Lactate","LABEL_TroponinI","LABEL_SaO2","LABEL_Bilirubin_direct","LABEL_EtCO2","LABEL_Sepsis","LABEL_RRate","LABEL_ABPm","LABEL_SpO2","LABEL_Heartrate"])
 
-
-
-##Subtask 2:
-#9.)    Train a model using the label sepsis (0 for no sepsis, 1 otherwise)
-
-
-#10.)   Predicting the occurance of sepsis with the standard function model.predict
-        #example:     if(model.predict([[X_FEATURES[i](Age), ..., X_FEATURES[i](pH) ]]))==0:
-
-
-#11.)   Writing the LABELS and the prediction for sepsis to the sample.csv file
-
-
-
-
-
-#file = open("sample_try.csv", "w")                                                  #Create submission file with writing access
-#number_of_features = 20
-
-
-#for i in range(number_of_features):                                             #Write stuff to the submission file
-#    file.write(str(np.nan))
-#    file.write('\n')
-#file.close()
+compression_opts = dict(method='zip', archive_name='sample.csv')
+M_Sub_panda.to_csv('sample.zip', index=False, float_format='%.3f', compression=compression_opts)
