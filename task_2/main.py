f57ca7c411586a706ee0252142fc80c4ec2b7e0a
Xenia Voellmy <xvoellmy@student.ethz.ch>
2020-04-15 10:39:27 +0200

    Unfinished version with CV Value

--- task_2/main.py
+++ task_2/main.py
@@ -25,7 +25,7 @@ def read_in_data(patients, activation_number_of_tests):
     mean_train = np.array(read_train_features.mean())                           #get mean and std over all patients for filling in nan's
     std_train = np.array(read_train_features.std())
     number_train_patients = int(read_train_features.shape[0]/12)
-    [X_TRAIN, pid_train] = pre_processing(number_train_patients, mean_train, std_train, read_train_features, activation_number_of_tests)        #pull X_TRAIN together for only one row per patient
+    [X_TRAIN, pid_train] = pre_processing_deterministic(number_train_patients, mean_train, std_train, read_train_features, activation_number_of_tests)        #pull X_TRAIN together for only one row per patient
 
     [X_TRAIN, Y_LABELS_1, Y_LABELS_2, Y_LABELS_3] = shuffle(X_TRAIN, Y_LABELS_1, Y_LABELS_2, Y_LABELS_3, n_samples=patients)      #shuffling and taking only the desired number of patients for training
 
@@ -35,13 +35,14 @@ def read_in_data(patients, activation_number_of_tests):
     mean_test = np.array(read_test_features.mean())
     std_test = np.array(read_test_features.std())
     number_test_patients = int(read_test_features.shape[0]/12)
-    [X_TEST, pid_test] = pre_processing(number_test_patients, mean_test, std_test, read_test_features, activation_number_of_tests)
+    [X_TEST, pid_test] = pre_processing_deterministic(number_test_patients, mean_test, std_test, read_test_features, activation_number_of_tests)
 
     return [pid_train, pid_test, Y_LABELS_1, Y_LABELS_2, Y_LABELS_3, X_TRAIN, X_TEST]
 
 def pre_processing(number_of_patients, mean, std, data_set, activation_number_of_tests):
     X_CUT = []
     pid = []
+
 
     for i in range(0,number_of_patients):                                       #going from 1 through all patients
         X_Patient = data_set.iloc[(12*i):(12*(i+1)),:]                          #extract only the relevant data for that specific patient
@@ -65,6 +66,34 @@ def pre_processing(number_of_patients, mean, std, data_set, activation_number_of
 
     return [X_CUT, pid]
 
+def pre_processing_deterministic(number_of_patients, mean, std, data_set, activation_number_of_tests):
+    X_CUT = []
+    pid = []
+
+    for i in range(0,number_of_patients):                                       #going from 1 through all patients
+        X_Patient = data_set.iloc[(12*i):(12*(i+1)),:]                          #extract only the relevant data for that specific patient
+        X_append = X_Patient.mean()
+        X_append = np.array(X_append)                                           #Has still all the columns in it
+        pid.append(X_append[0])
+        X_append = X_append[2:]                                                 #cut away the pid and the time (start from Age)
+        X_append = np.append(X_append, np.divide(X_Patient.std()[2:],X_Patient.mean()[2:]))
+
+        for j in range(0,int(len(X_append)/2)):
+            if ~np.isnan(X_append[j]) & np.isnan(X_append[int(len(X_append)/2)+j]):
+                X_append[int(len(X_append)/2)+j] = 0
+            if np.isnan(X_append[j]):
+                X_append[j] = mean[j+2]
+            if np.isnan(X_append[int(len(X_append)/2)+j]):
+                X_append[int(len(X_append)/2)+j] = std[j+2]/mean[j+2]
+
+        if activation_number_of_tests == True:
+            number_of_tests_made = 0
+            number_of_tests_made = np.count_nonzero(~np.isnan(np.array(X_Patient)[0:12,3:int(len(X_append)/2)+2]))     #number of non-NaN values in all tests over all hours
+            X_append = np.append(X_append, number_of_tests_made)
+
+        X_CUT.append(list(X_append))
+
+    return [X_CUT, pid]
 
 ###---------------MAIN----------------------------------------------------------
 patients = 50                           #number of patients to train the model
@@ -74,24 +103,25 @@ activation_number_of_tests = True       #if true it takes the number of tests ma
 X_TEST = np.nan_to_num(X_TEST)          #random stuff:)
 
 ##Subtask 1: Setting up a model with multiclass labels
+print(1)
 model_1 = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True))
 model_1.fit(np.array(X_TRAIN), np.array(Y_LABELS_1))
 y_pred_1 = model_1.predict_proba(X_TEST)                                        #Prediction
 y_pred_1_sigmoid = 1/(1 + np.exp(-model_1.decision_function(X_TEST)))
-
+print(2)
 ##Subtask 2: Setting up a model for sepsis
 model_2 = svm.SVC(kernel='linear', probability=True)
 model_2.fit(np.array(X_TRAIN), np.array(Y_LABELS_2))
 y_pred_2 = model_2.predict_proba(X_TEST)[:,1]                                   #Prediction
 y_pred_2_sigmoid = 1/(1 + np.exp(-model_2.decision_function(X_TEST)))
-
+print(3)
 ##Subtask 3: Setting up a model for mean of vital signs
 model_3 = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1], cv=None)                        #None for Leave-One-Out cross-validation
 model_3.fit(np.array(X_TRAIN), np.array(Y_LABELS_3))
 y_pred_3 = model_3.predict(X_TEST)                                              #Prediction
-
+print(4)
 #Creating submission matrix and writing to zip
-M_Sub = np.c_[pid_test, y_pred_1_sigmoid, y_pred_2_sigmoid, y_pred_3]
+M_Sub = np.c_[pid_test, y_pred_1, y_pred_2, y_pred_3]
 M_Sub_panda = pd.DataFrame(data=M_Sub, columns=["pid","LABEL_BaseExcess","LABEL_Fibrinogen","LABEL_AST","LABEL_Alkalinephos","LABEL_Bilirubin_total","LABEL_Lactate","LABEL_TroponinI","LABEL_SaO2","LABEL_Bilirubin_direct","LABEL_EtCO2","LABEL_Sepsis","LABEL_RRate","LABEL_ABPm","LABEL_SpO2","LABEL_Heartrate"])
 
 M_Sub_panda.to_csv(r'sample_' + str(patients) + '.csv', index = False)
