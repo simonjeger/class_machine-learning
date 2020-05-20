04e8bdb768d7b92f7d5c836f9e8fea3d43b976b4
Xenia Voellmy <xvoellmy@student.ethz.ch>
2020-05-20 08:32:12 +0200

    Task 3: Added Kfold

--- task_3/main_neural_network.py
+++ task_3/main_neural_network.py
@@ -4,6 +4,7 @@ import argparse
 from sklearn import svm
 from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.utils import class_weight
 from numpy.random import seed
 
@@ -14,7 +15,6 @@ from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.callbacks import Callback
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.utils import normalize, to_categorical
-#from tensorflow import set_random_seed
 
 
 print(tf.version)
@@ -66,7 +66,7 @@ class Val_F1(Callback):
 
         if val_f1 > self.best_val_f1:
             self.best_val_f1 = val_f1
-            self.model.save('best_model.h5')
+            self.model.save('best_model_kfolds.h5')
             print('------ improoooved ------')
 
         print('— val_f1: %f' %val_f1)
@@ -75,8 +75,8 @@ class Val_F1(Callback):
 ### Read in the whole data set
 [train_mutations, train_labels, test_mutations] = read_in_data()                #train_mutations and test_mutations contain all the four letters
 
-seed(1)
-#set_random_seed(2)
+seed(123)
+tf.random.set_seed(246)
 #convert letters to integers in 4 columns
 train_mutant = convert_letters_alternative(train_mutations)
 test_mutant = convert_letters_alternative(test_mutations)
@@ -84,47 +84,57 @@ test_mutant = convert_letters_alternative(test_mutations)
 train_mutant = np.asarray(train_mutant)                                         #Magic stuff to arrange array in nice matrix form:)
 train_labels = np.asarray(train_labels)
 test_mutant = np.asarray(test_mutant)
+test_labels = np.asarray(np.zeros(len(test_mutant)))
+print(len(test_labels))
 
-X_train, X_val, y_train, y_val = train_test_split(train_mutant, train_labels, test_size=0.25, random_state=42)
-#X_train = normalize(X_train, axis=1)
-#X_val = normalize(X_val, axis=1)
-#y_train = to_categorical(y_train)
-#y_val = to_categorical(y_val)
+n_folds = 5
+kf = KFold(n_splits=n_folds)
 
-#Final model with the optimal threshold value determined before
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(84,)))
-model.add(tf.keras.layers.Dense(400, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-#model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(200, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-#model.add(tf.keras.layers.Dense(80, activation='tanh'))
-#model.add(tf.keras.layers.Dropout(0.5))
-#model.add(tf.keras.layers.BatchNormalization())
-model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-model.summary()
+for train_index, test_index in kf.split(train_mutant):
+    X_train, X_val = train_mutant[train_index], train_mutant[test_index]
+    y_train, y_val = train_labels[train_index], train_labels[test_index]
+    #X_train = normalize(X_train, axis=1)
+    #X_val = normalize(X_val, axis=1)
+    #y_train = to_categorical(y_train)
+    #y_val = to_categorical(y_val)
+    print('---Started model training---')
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #Final model with the optimal threshold value determined before
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(84,)))
+    model.add(tf.keras.layers.Dense(196, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    #model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dense(98, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    #model.add(tf.keras.layers.Dense(80, activation='tanh'))
+    #model.add(tf.keras.layers.Dropout(0.5))
+    #model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+    model.summary()
 
-f1_metric = Val_F1(X_val,y_val)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-weights = class_weight.compute_class_weight('balanced', np.unique(train_labels), train_labels)
-weights = dict(enumerate(weights))
+    f1_metric = Val_F1(X_val,y_val)
 
-model.fit(X_train, y_train, epochs=60, class_weight=weights, callbacks=[f1_metric],verbose=1)
+    weights = class_weight.compute_class_weight('balanced', np.unique(train_labels), train_labels)
+    weights = dict(enumerate(weights))
 
-#test_mutant = normalize(test_mutant, axis=1)
-final_model = load_model('best_model.h5')
+    model.fit(X_train, y_train,validation_data=(X_val,y_val), epochs=30, class_weight=weights, callbacks=[f1_metric],verbose=1)
 
-print('predictions')
-test_labels = final_model.predict(test_mutant)
+    #test_mutant = normalize(test_mutant, axis=1)
+    final_model = load_model('best_model_kfolds.h5')
+
+    print('predictions')
+    test_labels += final_model.predict(test_mutant).reshape(-1)
+    tf.keras.backend.clear_session()
+
 
 print('rounding')
-test_labels = np.round(test_labels).reshape(-1)
+test_labels = np.round(np.divide(test_labels,n_folds))
 
 print('write csv file')
-np.savetxt('sample_neural.csv', test_labels, delimiter='\n', fmt ='%d')
+np.savetxt('sample_neural_kfolds.csv', test_labels, delimiter='\n', fmt ='%d')
 #M_submission_pd = pd.DataFrame(data=test_labels)
 #M_submission_pd.to_csv(r'sample_neural.csv', index=False)
 #compression_opts = dict(method='zip', archive_name='sample.csv')
